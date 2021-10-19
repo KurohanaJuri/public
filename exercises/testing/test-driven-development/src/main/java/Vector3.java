d39b8e730ad39c48c2f11b081ec6d2fdb8f06c9a
Chau Ying Kot <chau-ying-kot@hotmail.com>
2021-10-19 22:34:04 +0200

    Exercices

--- exercises/testing/test-driven-development/src/main/java/Vector3.java
+++ exercises/testing/test-driven-development/src/main/java/Vector3.java
@@ -1,7 +1,86 @@
+import java.util.Arrays;
 
 /**
  * Write the specification here
  */
 public final class Vector3 {
 
+    private double[] v = {0, 0, 0};
+
+    public Vector3(double x, double y, double z) {
+        this.v[0] = x;
+        this.v[1] = y;
+        this.v[2] = z;
+    }
+
+    public double getX() {
+        return v[0];
+    }
+
+    public double getY() {
+        return v[1];
+    }
+
+    public double getZ() {
+        return v[2];
+    }
+
+    public double dot(Vector3 v2) {
+        int res = 0;
+        for (int i = 0; i < 3; i++) {
+            res += this.v[i] * v2.v[i];
+        }
+        return res;
+    }
+
+
+    public double length() {
+        int res = 0;
+        for (int i = 0; i < 3; i++) {
+            res += this.v[i] * this.v[i];
+        }
+        return Math.sqrt(res);
+    }
+
+    public Vector3 cross(Vector3 v2) {
+        return new Vector3(this.v[1] * v2.v[2] - this.v[2] * v2.v[1],
+                this.v[2] * v2.v[0] - this.v[0] * v2.v[2],
+                this.v[0] * v2.v[1] - this.v[1] * v2.v[0]);
+    }
+
+    public Vector3 norm() {
+        if (this.length() == 0) {
+            return new Vector3(0, 0, 0);
+        }
+
+        return new Vector3(this.v[0] / this.length(),
+                this.v[1] / this.length(),
+                this.v[2] / this.length());
+    }
+
+    public Vector3 scale(int lambda) {
+        return new Vector3(this.v[0] * lambda,
+                this.v[1] * lambda,
+                this.v[2] * lambda);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector3 vector3 = (Vector3) o;
+        return Arrays.equals(v, vector3.v);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(v);
+    }
+
+    @Override
+    public String toString() {
+        return "Vector3{" +
+                "v=" + Arrays.toString(v) +
+                '}';
+    }
 }
--- exercises/testing/test-driven-development/src/test/java/Vector3Test.java
+++ exercises/testing/test-driven-development/src/test/java/Vector3Test.java
@@ -1,12 +1,65 @@
 import org.junit.jupiter.api.Test;
+
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 final class Vector3Test {
 
     @Test
-    public void sampleTest() {
-        assertThat(1, is(1));
+    public void isZero() {
+        Vector3 v = new Vector3(0, 0, 0);
+        assertThat(v.getX(), is(0.0));
+        assertThat(v.getY(), is(0.0));
+        assertThat(v.getZ(), is(0.0));
     }
 
+    @Test
+    public void dotProductShouldReturnTheCorrectValue() {
+        Vector3 v1 = new Vector3(1, 2, 3);
+        Vector3 v2 = new Vector3(4, 5, 6);
+        assertThat(v1.dot(v2), is(32.0));
+    }
+
+    @Test
+    public void dotProductWithAZeroVectorReturnZero() {
+        Vector3 v1 = new Vector3(0, 0, 0);
+        Vector3 v2 = new Vector3(4, 5, 6);
+        assertThat(v1.dot(v2), is(0.0));
+    }
+
+    @Test
+    public void theVectorLengthShouldBeCorrect() {
+        Vector3 v = new Vector3(2, 2, 1);
+        assertThat(v.length(), is(3.0));
+    }
+
+    @Test
+    public void crossProductShouldBeCorrect() {
+        Vector3 v1 = new Vector3(1, 2, 3);
+        Vector3 v2 = new Vector3(4, 5, 6);
+        // x = 2*6-3*5 = 12 -15
+        // y = 3*4-1*6 = 12 - 6
+        // z = 1*5-2*4 = 5-8
+        assertThat(v1.cross(v2), is(new Vector3(-3, 6, -3)));
+    }
+
+    @Test
+    public void normalizeShouldBeCorrect() {
+        Vector3 v1 =  new Vector3(2, 2, 1);
+        assertThat(v1.norm(), is(new Vector3(2./3, 2./3, 1./3)));
+    }
+
+    @Test
+    public void normalizeWithALengthZeroShouldReturnZero() {
+        Vector3 v1 = new Vector3(0, 0, 0);
+        assertThat(v1.norm(), is(new Vector3(0, 0, 0)));
+    }
+
+    @Test
+    public void scaleShouldBeCorrect() {
+        Vector3 v1 = new Vector3(1, 2, 3);
+        assertThat(v1.scale(5), is(new Vector3(5, 10, 15)));
+    }
 }
